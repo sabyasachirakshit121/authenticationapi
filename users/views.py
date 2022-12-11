d27c81244775866f3a6a7909a33e7d18c4a10b7f
Sabyasachi Rakshit <sabyasachirakshitacs19@gmail.com>
2022-12-11 18:56:10 +0530

    added urls

--- users/views.py
+++ users/views.py
@@ -2,7 +2,7 @@ from django.contrib.auth import get_user_model, logout
 from django.core.exceptions import ImproperlyConfigured
 from rest_framework import viewsets, status
 from rest_framework.decorators import action
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 
 from . import serializers
@@ -16,7 +16,8 @@ class AuthViewSet(viewsets.GenericViewSet):
     serializer_class = serializers.EmptySerializer
     serializer_classes = {
         'login': serializers.UserLoginSerializer,
-        'register': serializers.UserRegisterSerializer
+        'register': serializers.UserRegisterSerializer,
+        'password_change': serializers.PasswordChangeSerializer,
     }
 
     @action(methods=['POST', ], detail=False)
@@ -40,6 +41,14 @@ class AuthViewSet(viewsets.GenericViewSet):
         logout(request)
         data = {'success': 'Sucessfully logged out'}
         return Response(data=data, status=status.HTTP_200_OK)
+
+    @action(methods=['POST'], detail=False, permission_classes=[IsAuthenticated, ])
+    def password_change(self, request):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        request.user.set_password(serializer.validated_data['new_password'])
+        request.user.save()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
     def get_serializer_class(self):
         if not isinstance(self.serializer_classes, dict):
