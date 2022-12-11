d12fb2abb8ea9d7367828f497c33d587707004cb
Sabyasachi Rakshit <sabyasachirakshitacs19@gmail.com>
2022-12-11 18:49:55 +0530

    added register method in views

--- users/views.py
+++ users/views.py
@@ -6,7 +6,7 @@ from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 
 from . import serializers
-from .utils import get_and_authenticate_user
+from .utils import get_and_authenticate_user, create_user_account
 
 User = get_user_model()
 
@@ -16,6 +16,7 @@ class AuthViewSet(viewsets.GenericViewSet):
     serializer_class = serializers.EmptySerializer
     serializer_classes = {
         'login': serializers.UserLoginSerializer,
+        'register': serializers.UserRegisterSerializer
     }
 
     @action(methods=['POST', ], detail=False)
@@ -25,6 +26,14 @@ class AuthViewSet(viewsets.GenericViewSet):
         user = get_and_authenticate_user(**serializer.validated_data)
         data = serializers.AuthUserSerializer(user).data
         return Response(data=data, status=status.HTTP_200_OK)
+
+    @action(methods=['POST', ], detail=False)
+    def register(self, request):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = create_user_account(**serializer.validated_data)
+        data = serializers.AuthUserSerializer(user).data
+        return Response(data=data, status=status.HTTP_201_CREATED)
 
     def get_serializer_class(self):
         if not isinstance(self.serializer_classes, dict):
